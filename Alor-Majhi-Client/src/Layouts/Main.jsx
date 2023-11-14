c55d8574bf44ad62310ae92cd75bf0c10dfcbf8a
Md Saiful Islam <sheikhmsip@gmail.com>
2023-11-15 01:00:32 +0600

    added apply membership button

--- Alor-Majhi-Client/src/Layouts/Main.jsx
+++ Alor-Majhi-Client/src/Layouts/Main.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from '../Components/Navbar/Navbar';
-import Footer from '../Components/Footer/Footer';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
 
 const Main = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-           <Outlet></Outlet>
-           <Footer></Footer>
-        </div>
-    );
+  return (
+    <div className="">
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
